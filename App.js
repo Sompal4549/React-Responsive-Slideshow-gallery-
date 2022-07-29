9651f909e4ce416e086c780c760fcea619e5463f
Sompal Kumar <96425978+Sompal4549@users.noreply.github.com>
2022-07-29 21:23:22 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,13 @@
+import { slides } from "../data/data";
+import Slider from "./../components/Slider";
+export default function App() {
+  const containerStyle = {
+    width: "500px",
+    height: "280px",
+    margin: "0 auto",
+  };
+  return(
+ <div style={containerStyle}>
+   <Slider slides={slides} />
+</div>
+)
